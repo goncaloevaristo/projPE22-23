97b50bd2c24ab288a972f5d2de497417abb33efa
Gonçalo Evaristo <119701258+goncaloevaristo@users.noreply.github.com>
2023-06-08 15:26:56 +0100

    Add files via upload

--- 10.R
+++ 10.R
@@ -1,10 +1,29 @@
-set.seed(1187)  # Fixar a semente em 1187
-m <- 200  # Número de amostras
-n <- 39  # Dimensão de cada amostra
-mu <- 51.3  # Valor esperado conhecido
-sigma <- 2  # Desvio padrão conhecido
-alpha <- 0.1  # Nível de significância
-samples <- matrix(rnorm(m * n, mean = mu, sd = sigma), nrow = m, ncol = n) #gerar amostras
-sample_means <- rowMeans(samples) #média de cada amostra
-null_hypothesis_accepted <- sum(abs(sample_means - 50.1) <= qnorm(1 - alpha/2) * sigma/sqrt(n)) #contar quantas vezes a hipótese nula é aceite
-probability_accept_H0 <- null_hypothesis_accepted / m #calcular a probabilidade
+# Fixar a semente em 1187
+set.seed(1187)
+
+# Número de amostras
+m <- 200
+
+# Dimensão de cada amostra
+n <- 39
+
+# Valor esperado conhecido
+mu <- 51.3
+
+#Desvio padrão conhecido
+sigma <- 2
+
+# Nível de significância
+alpha <- 0.1
+
+#gerar amostras
+samples <- matrix(rnorm(m * n, mean = mu, sd = sigma), nrow = m, ncol = n)
+
+#média de cada amostra
+sample_means <- rowMeans(samples)
+
+#contar quantas vezes a hipótese nula é aceite
+null_hypothesis_accepted <- sum(abs(sample_means - 50.1) <= qnorm(1 - alpha/2) * sigma/sqrt(n))
+
+#calcular a probabilidade
+probability_accept_H0 <- null_hypothesis_accepted / m
