8c60aea4597615034176bd3bf025642c0c0ea0df
Gonçalo Evaristo <119701258+goncaloevaristo@users.noreply.github.com>
2023-07-08 17:56:59 +0100

    Update 5.R

--- 5.R
+++ 5.R
@@ -1,23 +1,27 @@
-# fixar a semente
+# Fixar a semente para garantir a reprodutibilidade dos resultados
 set.seed(1891)
 
-# gerar amostra de tamanho n
+# Gerar amostra de tamanho n
 n <- 1173
-# definir parâmetro p
+
+# Definir parâmetro p
 p <- 0.35
 
-u <- runif(n)  # simular valores de uma distribuição uniforme (0,1)
-x <- ceiling(log(1-u) / log(1-p))  # aplicar o método de transformação inversa
+# Simular valores de uma distribuição uniforme (0,1)
+u <- runif(n)
 
-# calcular média e desvio padrão amostrais
+# Aplicar o método de transformação inversa para obter valores da distribuição desejada
+x <- ceiling(log(1-u) / log(1-p))
+
+# Calcular a média e o desvio padrão amostrais
 mean_x <- mean(x)
 sd_x <- sd(x)
 
-# contar quantos valores são maiores que a soma da média e do desvio padrão
+# Contar quantos valores são maiores que a soma da média e do desvio padrão
 count <- sum(x > mean_x + sd_x)
 
-# calcular proporção
+# Calcular a proporção
 proporcao <- count / sum(x > mean_x)
 
-# apresentar resultado com 4 casas decimais
+# Apresentar o resultado com 4 casas decimais
 round(proporcao, 4)
